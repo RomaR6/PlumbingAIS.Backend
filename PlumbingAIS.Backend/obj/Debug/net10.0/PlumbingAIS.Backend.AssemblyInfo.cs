63729749b594c41a788c0d0c2f28fbe05d1d2318
OleksiukRoman <romanoleksuk10@gmail.com>
2026-04-04 18:12:56 +0300

    Downgrade to .NET 9.0 and install EF Core MySQL packages

--- PlumbingAIS.Backend/obj/Debug/net10.0/PlumbingAIS.Backend.AssemblyInfo.cs
+++ PlumbingAIS.Backend/obj/Debug/net10.0/PlumbingAIS.Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlumbingAIS.Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98cb44a3589ee2a1f046a21fb32c42745a51ca03")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlumbingAIS.Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlumbingAIS.Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
