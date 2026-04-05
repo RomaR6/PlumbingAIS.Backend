9dadc46ca72fcde65c8aa5f1899dbe7851bd667c
OleksiukRoman <romanoleksuk10@gmail.com>
2026-04-05 13:13:55 +0300

    Add ProductsController and test GET request from DB

---
+++ PlumbingAIS.Backend/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PlumbingAIS.Backend.Data;
+using PlumbingAIS.Backend.Models;
+
+namespace PlumbingAIS.Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProductsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            return await _context.Products
+                .Include(p => p.Category)
+                .ToListAsync();
+        }
+    }
+}
