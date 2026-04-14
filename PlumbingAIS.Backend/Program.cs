d4daca0f7ab1e725df66aad385645b0006964aa8
OleksiukRoman <romanoleksuk10@gmail.com>
2026-04-14 20:42:04 +0300

    Implement Generic Repository pattern for unified CRUD operations

--- PlumbingAIS.Backend/Program.cs
+++ PlumbingAIS.Backend/Program.cs
@@ -16,6 +16,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend",
